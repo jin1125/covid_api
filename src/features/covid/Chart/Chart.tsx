43dac9b1b4de2cace678ad269ab3f11d1a6238ef
jin1125 <shogo.jinta@gmail.com>
2021-08-28 19:13:01 +0900

    finish

--- src/features/covid/Chart/Chart.tsx
+++ src/features/covid/Chart/Chart.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import styles from "./Chart.module.css";
+import { Line } from "react-chartjs-2";
+
+import { useSelector } from "react-redux";
+import { selectDaily } from "../covidSlice";
 
 const Chart:React.FC = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const daily = useSelector(selectDaily);
+  const dates = daily.map(({ Date }) => Date);
+
+  const lineChart = daily[0] && (
+    <Line
+      data={{
+        labels: dates.map((date) => new Date(date).toDateString()),
+
+        datasets: [
+          {
+            data: daily.map((data) => data.Confirmed),
+            label: "目をつけられた人",
+            borderColor: "#3333ff",
+            showLine: false,
+          },
+          {
+            data: daily.map((data) => data.Recovered),
+            label: "逃れた人",
+            borderColor: "green",
+            showLine: false,
+          },
+          {
+            data: daily.map((data) => data.Deaths),
+            label: "食べられた人",
+            borderColor: "#ff3370",
+            showLine: false,
+          },
+        ],
+      }}
+    />
+  );
+
+
+   return <div className={styles.container}>{lineChart}</div>;
 }
 
 export default Chart
